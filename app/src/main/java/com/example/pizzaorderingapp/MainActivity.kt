c10771d7b62931a73f6ae4a3ec59981c56914908
YuenShanTsang <A00270323@mycambrian.ca>
2023-04-16 17:45:46 -0400

    save test

--- app/src/main/java/com/example/pizzaorderingapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderingapp/MainActivity.kt
@@ -1,25 +1,22 @@
 package com.example.pizzaorderingapp
 
-import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.CheckedTextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.pizzaorderingapp.databinding.ActivityMainBinding
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     // View binding
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -27,105 +24,77 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Set up the RecyclerView with the ToppingAdapter and the list of Topping enum values
-        val toppings = Topping.values().toList()
-        binding.toppingListRecyclerView.adapter = ToppingAdapter(toppings)
-        binding.toppingListRecyclerView.layoutManager = LinearLayoutManager(this)
+        // Set up topping list view
+        val adapter = ToppingAdapter(this)
+        binding.toppingListView.adapter = adapter
 
-        // Submit button
+        // Set up submit button click listener
         binding.submitButton.setOnClickListener {
-            // Iterate over the toppings in the adapter and add the selected ones to the list
-            val adapter = binding.toppingListRecyclerView.adapter as ToppingAdapter
-            val selectedToppings = mutableListOf<Topping>()
-            for (i in 0 until adapter.itemCount) {
-                val holder =
-                    binding.toppingListRecyclerView.findViewHolderForAdapterPosition(i) as? ToppingAdapter.ViewHolder
-                if (holder != null && holder.toppingName.isChecked) {
-                    selectedToppings.add(adapter.toppings[i])
+            val selectedToppings = adapter.getSelectedToppings()
+            val selectedToppingsString = selectedToppings.joinToString(", ")
+            Toast.makeText(this, "Selected toppings: $selectedToppingsString", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, PastOrder::class.java)
+            intent.putStringArrayListExtra("selectedToppings", ArrayList(selectedToppings))
+            startActivity(intent)
+        }
+
+    }
+
+    // Topping items
+    enum class Topping(val toppings: String) {
+        Pepperoni("Pepperoni"),
+        Mushroom("Mushroom"),
+        ExtraCheese("Extra Cheese"),
+        Sausage("Sausage"),
+        Onion("Onion"),
+        Bacon("Bacon"),
+        Ham("Ham"),
+        Pineapple("Pineapple"),
+        GreenPepper("Green Pepper"),
+        BlackOlives("Black Olives")
+    }
+
+    class ToppingAdapter(private val context: Context) : BaseAdapter() {
+
+        private val selectedToppings = mutableListOf<String>()
+
+        override fun getCount(): Int {
+            return Topping.values().size
+        }
+
+        override fun getItem(position: Int): Any {
+            return Topping.values()[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.topping_item, parent, false)
+
+            val toppingName = view.findViewById<CheckedTextView>(R.id.topping_name)
+            val topping = getItem(position) as Topping
+            toppingName.text = topping.toppings
+            toppingName.isChecked = selectedToppings.contains(topping.toppings)
+
+            view.setOnClickListener {
+                val isChecked = !selectedToppings.contains(topping.toppings)
+                if (isChecked) {
+                    selectedToppings.add(topping.toppings)
+                } else {
+                    selectedToppings.remove(topping.toppings)
                 }
+                toppingName.isChecked = isChecked
             }
 
-            if (selectedToppings.isNotEmpty()) {
-                // Create the order object
-                val order = Order(selectedToppings)
-
-                // Process the order
-                OrderProcessor.processOrder(this, order)
-
-                // Write the selected toppings to a file
-                val file = File(this.filesDir, "selected_toppings.txt")
-                file.writeText(selectedToppings.joinToString(",") { it.toppings })
-
-                // Clear the selected toppings list and show a successful message
-                selectedToppings.clear()
-                (binding.toppingListRecyclerView.adapter as? ToppingAdapter)?.notifyDataSetChanged()
-                Toast.makeText(this, "Order submitted successfully!", Toast.LENGTH_SHORT).show()
-
-                // Take user input
-                val inputText = binding.inputEditText.text.toString()
-
-                // Start the PastOrder activity with the selected toppings
-                val intent = Intent(this, PastOrder::class.java)
-                intent.putExtra(
-                    "selectedToppings",
-                    selectedToppings.map { it.toppings }.toTypedArray()
-                )
-                intent.putExtra("inputText", inputText)
-                startActivity(intent)
-
-            } else {
-                // Make sure the list is not empty
-                Toast.makeText(this, "Please select at least one topping!", Toast.LENGTH_SHORT)
-                    .show()
-            }
-
+            return view
         }
 
-    }
-
-}
-
-// Topping items
-enum class Topping(val toppings: String) {
-    Pepperoni("Pepperoni"),
-    Mushroom("Mushroom"),
-    ExtraCheese("Extra Cheese"),
-    Sausage("Sausage"),
-    Onion("Onion")
-}
-
-// Adapter
-class ToppingAdapter(internal val toppings: List<Topping>) :
-    RecyclerView.Adapter<ToppingAdapter.ViewHolder>() {
-
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val toppingName: CheckedTextView = view.findViewById(R.id.topping_name)
-
-        init {
-            // Add OnClickListener to toggle the checkbox state
-            // When the user clicks the topping name, the checkbox state will be changed
-            toppingName.setOnClickListener {
-                toppingName.isChecked = !toppingName.isChecked
-            }
+        fun getSelectedToppings(): List<String> {
+            return selectedToppings
         }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // Inflate the layout for the topping item
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.topping_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // Get the topping at the current position
-        val topping = toppings[position]
-
-        // Set the topping name in the corresponding CheckedTextView
-        holder.toppingName.text = topping.toppings
-    }
-
-    override fun getItemCount(): Int {
-        // Return the number of toppings in the list
-        return toppings.size
-    }
 }
+
--- app/src/main/java/com/example/pizzaorderingapp/PastOrder.kt
+++ app/src/main/java/com/example/pizzaorderingapp/PastOrder.kt
@@ -1,18 +1,10 @@
 package com.example.pizzaorderingapp
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.pizzaorderingapp.databinding.ActivityPastOrderBinding
-import java.io.File
-import com.google.gson.Gson
 
 class PastOrder : AppCompatActivity() {
 
@@ -25,112 +17,27 @@ class PastOrder : AppCompatActivity() {
         // Setup view binding
         binding = ActivityPastOrderBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.selectedToppingsRecyclerView.layoutManager = LinearLayoutManager(this)
 
-        // Set the selected toppings in the RecyclerView
-        val toppingFile = File(this.filesDir, "selected_toppings.txt")
-        if (toppingFile.exists()) {
-            val selectedToppings = toppingFile.readText().split(",")
+        val selectedToppings = intent.getStringArrayListExtra("selectedToppings")
 
-            // Create a new adapter for the RecyclerView
-            val recyclerViewAdapter = MyRecyclerViewAdapter(selectedToppings)
+        // Display the selected toppings
+        binding.requestedTextView.text = selectedToppings?.joinToString()
 
-            // Set the adapter for the RecyclerView
-            binding.selectedToppingsRecyclerView.adapter = recyclerViewAdapter
-        } else {
-            toppingFile.createNewFile()
-            binding.selectedToppingsRecyclerView.adapter = null
-        }
+        // Concatenate the selected toppings into a single string
+        val toppingsString = selectedToppings?.joinToString()
 
-        // Display inputted text
-        val inputText = intent.getStringExtra("inputText")
-        if (inputText != null) {
-            binding.requestedTextView.text = inputText
-        }
+        // Create an adapter to display the selected toppings in the ListView
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_multiple_choice,
+            listOf(toppingsString)
+        )
 
-        // Set click listeners for the buttons
-        binding.changeButton.setOnClickListener {
-            // Get the current selected toppings from the file
-            val file = File(this.filesDir, "selected_toppings.txt")
-            val selectedToppings = file.readText().split(",")
+        // Set the adapter on the ListView
+        binding.selectedToppingsListView.adapter = adapter
 
-            // Launch the MainActivity with the current selected toppings as an intent extra
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("selectedToppings", selectedToppings.toTypedArray())
-            startActivity(intent)
-        }
-
-        binding.deleteButton.setOnClickListener {
-            // (Handle delete here)
-        }
-    }
-}
-
-// Data class for holding the selected toppings for an order
-data class Order(val toppings: List<Topping>)
-
-// Helper object for processing orders
-object OrderProcessor {
-    private const val FILENAME = "orders.txt"
-
-    private fun saveOrders(context: Context, orders: List<Order>) {
-        // Save orders as JSON strings in a text file
-        val file = File(getOrdersFilepath(context))
-        val ordersJson = orders.map { Gson().toJson(it) }
-        file.writeText(ordersJson.joinToString("\n"))
+        // Enable multiple item selection on the ListView
+        binding.selectedToppingsListView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
     }
 
-    fun processOrder(context: Context, order: Order) {
-        // Load orders, add the new order, and save the updated orders
-        val orders = loadOrders(context)
-        orders.add(order)
-        saveOrders(context, orders)
-    }
-
-    private fun loadOrders(context: Context): MutableList<Order> {
-        // Load orders from the text file and parse as JSON strings
-        val file = File(getOrdersFilepath(context))
-        if (!file.exists()) {
-            file.createNewFile()
-        }
-        val orderJsonList = file.readLines()
-        val orders = mutableListOf<Order>()
-        orderJsonList.forEach {
-            val order = Gson().fromJson(it, Order::class.java)
-            orders.add(order)
-        }
-        return orders
-    }
-
-    private fun getOrdersFilepath(context: Context): String {
-        // Return the absolute path for the orders text file
-        return File(context.filesDir, FILENAME).absolutePath
-    }
-}
-
-// RecyclerView adapter for displaying the selected toppings in a past order
-class MyRecyclerViewAdapter(private val items: List<String>) :
-    RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>() {
-
-    // Create a new ViewHolder by inflating the past_order layout
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.past_order, parent, false)
-        return ViewHolder(view)
-    }
-
-    // Bind data to the views in the ViewHolder for the given position
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
-    }
-
-    // Return the number of items in the data set
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    // Define the ViewHolder, which holds references to views in the past_order layout
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.past_order_item_checkbox)
-    }
 }
