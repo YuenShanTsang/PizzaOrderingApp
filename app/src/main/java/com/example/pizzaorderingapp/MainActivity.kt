39c2f0a2b9c4b5348029b3ce43d7a912bed66d85
YuenShanTsang <A00270323@mycambrian.ca>
2023-04-20 05:46:59 -0400

    save progress

--- app/src/main/java/com/example/pizzaorderingapp/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderingapp/MainActivity.kt
@@ -30,9 +30,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sharedPreferences = getSharedPreferences("orders", Context.MODE_PRIVATE)
+
         // Set up topping list view
         val adapter = ToppingAdapter(this)
         binding.toppingListView.adapter = adapter
+
+        // Retrieve previously selected toppings from shared preferences
+        if (intent.getBooleanExtra("is_editing", false)) {
+            val ordersJson = sharedPreferences.getString("orders", "[]")
+            val orders = Gson().fromJson(ordersJson, Array<Order>::class.java)
+            if (orders.isNotEmpty()) {
+                val lastOrder = orders.last()
+                val selectedToppings = lastOrder.toppings
+                // Add the selected toppings to the adapter
+                for (topping in selectedToppings) {
+                    adapter.addSelectedTopping(topping)
+                }
+                // Calculate total price and update price text view
+                var totalPrice = 0.0
+                for (topping in selectedToppings) {
+                    totalPrice += Topping.valueOf(topping).price
+                }
+                binding.priceTextView.text = String.format("$%.2f", totalPrice)
+            }
+        }
 
         // Set up submit button click listener
         binding.submitButton.setOnClickListener {
@@ -44,85 +66,91 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Successful!", Toast.LENGTH_SHORT).show()
 
                 // Save the order in shared preferences
-                val ordersJson = sharedPreferences.getString("orders", "[]")
-                val orders = Gson().fromJson(ordersJson, Array<PastOrder.Order>::class.java).toMutableList()
-                orders.add(PastOrder.Order(selectedToppings))
-                val newOrdersJson = Gson().toJson(orders)
-                sharedPreferences.edit().putString("orders", newOrdersJson).apply()
+                val ordersJsonStr = sharedPreferences.getString("orders", "[]")
+                val orders = Gson().fromJson(ordersJsonStr, Array<Order>::class.java).toMutableList()
+                orders.add(Order(selectedToppings))
+                val newOrdersJsonStr = Gson().toJson(orders)
+                sharedPreferences.edit().putString("orders", newOrdersJsonStr).apply()
 
                 val intent = Intent(this, PastOrder::class.java)
                 intent.putStringArrayListExtra("selectedToppings", ArrayList(selectedToppings))
                 startActivity(intent)
             }
         }
-
-
-        sharedPreferences = getSharedPreferences("orders", Context.MODE_PRIVATE)
-    }
-
-    // Topping items
-    enum class Topping(val toppings: String, val price: Double) {
-        Pepperoni("Pepperoni", 0.99),
-        Mushroom("Mushroom", 0.79),
-        ExtraCheese("Extra Cheese", 1.29),
-        Sausage("Sausage", 1.49),
-        Onion("Onion", 0.69),
-        Bacon("Bacon", 1.79),
-        Ham("Ham", 1.29),
-        Pineapple("Pineapple", 0.89),
-        GreenPepper("Green Pepper", 0.69),
-        BlackOlives("Black Olives", 0.99)
     }
 
 
-    class ToppingAdapter(private val context: Context) : BaseAdapter() {
 
-        private val selectedToppings = mutableListOf<String>()
 
-        private var totalPrice = 0.0
 
-        override fun getCount(): Int {
-            return Topping.values().size
-        }
 
-        override fun getItem(position: Int): Any {
-            return Topping.values()[position]
-        }
-
-        override fun getItemId(position: Int): Long {
-            return position.toLong()
-        }
-
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-            val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.topping_item, parent, false)
-
-            val priceText: TextView = (context as MainActivity).findViewById(R.id.price_text_view)
-
-            val toppingName = view.findViewById<CheckedTextView>(R.id.topping_name)
-            val topping = getItem(position) as Topping
-            toppingName.text = "${topping.toppings} - $${topping.price}"
-            toppingName.isChecked = selectedToppings.contains(topping.toppings)
-
-            view.setOnClickListener {
-                val isChecked = !selectedToppings.contains(topping.toppings)
-                if (isChecked) {
-                    selectedToppings.add(topping.toppings)
-                    totalPrice += topping.price
-                } else {
-                    selectedToppings.remove(topping.toppings)
-                    totalPrice -= topping.price
-                }
-                toppingName.isChecked = isChecked
-                priceText.text = String.format("$%.2f", totalPrice)
-
-            }
-
-            return view
-        }
-
-        fun getSelectedToppings(): List<String> {
-            return selectedToppings
-        }
-    }
 }
 
+// Topping items
+enum class Topping(val toppings: String, val price: Double) {
+    Pepperoni("Pepperoni", 0.99),
+    Mushroom("Mushroom", 0.79),
+    Tomato("Tomato", 1.29),
+    Sausage("Sausage", 1.49),
+    Onion("Onion", 0.69),
+    Bacon("Bacon", 1.79),
+    Ham("Ham", 1.29),
+    Pineapple("Pineapple", 0.89),
+    GreenPepper("Green Pepper", 0.69),
+    BlackOlives("Black Olives", 0.99)
+}
+class ToppingAdapter(private val context: Context) : BaseAdapter() {
+
+    private val selectedToppings = mutableListOf<String>()
+
+    private var totalPrice = 0.0
+
+    override fun getCount(): Int {
+        return Topping.values().size
+    }
+
+    override fun getItem(position: Int): Any {
+        return Topping.values()[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.topping_item, parent, false)
+
+        val priceText: TextView = (context as MainActivity).findViewById(R.id.price_text_view)
+
+        val toppingName = view.findViewById<CheckedTextView>(R.id.topping_name)
+        val topping = getItem(position) as Topping
+        toppingName.text = "${topping.toppings} - $${topping.price}"
+        toppingName.isChecked = selectedToppings.contains(topping.toppings)
+
+        view.setOnClickListener {
+            val isChecked = !selectedToppings.contains(topping.toppings)
+            if (isChecked) {
+                selectedToppings.add(topping.toppings)
+                totalPrice += topping.price
+            } else {
+                selectedToppings.remove(topping.toppings)
+                totalPrice -= topping.price
+            }
+            toppingName.isChecked = isChecked
+            priceText.text = String.format("$%.2f", totalPrice)
+
+        }
+
+        return view
+    }
+
+    fun getSelectedToppings(): List<String> {
+        return selectedToppings
+    }
+
+    fun addSelectedTopping(topping: String) {
+        selectedToppings.add(topping)
+        notifyDataSetChanged() // update the view
+    }
+
+}
--- app/src/main/java/com/example/pizzaorderingapp/PastOrder.kt
+++ app/src/main/java/com/example/pizzaorderingapp/PastOrder.kt
@@ -1,12 +1,15 @@
 package com.example.pizzaorderingapp
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.CheckedTextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pizzaorderingapp.databinding.ActivityPastOrderBinding
 import com.google.gson.Gson
@@ -16,6 +19,14 @@ class PastOrder : AppCompatActivity() {
     // View binding
     private lateinit var binding: ActivityPastOrderBinding
 
+    // List of orders retrieved from shared preferences
+    private val orders: List<Order> by lazy {
+        getSavedOrders()
+    }
+
+    // Selected order position
+    private var selectedOrderPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,30 +34,93 @@ class PastOrder : AppCompatActivity() {
         binding = ActivityPastOrderBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Retrieve the saved orders from shared preferences
-        val ordersJson = getSharedPreferences("orders", Context.MODE_PRIVATE).getString("orders", "[]")
-        val orders = Gson().fromJson(ordersJson, Array<Order>::class.java)
-
         // Create an adapter to display the orders in the ListView
-        val adapter = OrderAdapter(this, orders.toList())
+        val adapter = OrderAdapter(this, orders, selectedOrderPosition)
 
         // Set the adapter on the ListView
         binding.selectedToppingsListView.adapter = adapter
+
+
+        binding.selectedToppingsListView.setOnItemClickListener { _, _, position, _ ->
+            orders.forEach { it.isChecked = false }
+            orders[position].isChecked = true
+            selectedOrderPosition = position
+            adapter.notifyDataSetChanged()
+        }
+
+        binding.editButton.setOnClickListener {
+            if (selectedOrderPosition >= 0) {
+                // Retrieve the selected toppings from the order
+                val selectedToppings = orders[selectedOrderPosition].toppings
+
+                // Pass the selected toppings back to the main activity for editing
+                val returnIntent = Intent()
+                returnIntent.putExtra("selected_toppings", selectedToppings.toTypedArray())
+                setResult(Activity.RESULT_OK, returnIntent)
+
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("is_editing", true) // add this line
+                startActivity(intent)
+
+                // Move finish() call to after startActivity()
+                finish()
+            } else {
+                Toast.makeText(this, "Please select an order to edit", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+
+
+        binding.deleteButton.setOnClickListener {
+            if (selectedOrderPosition >= 0) {
+                val updatedOrders = orders.toMutableList()
+                updatedOrders.removeAt(selectedOrderPosition)
+                updateSavedOrders(updatedOrders)
+                adapter.clear()
+                adapter.addAll(updatedOrders)
+                adapter.notifyDataSetChanged()
+                selectedOrderPosition = -1
+            } else {
+                Toast.makeText(this, "Please select an order to delete", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
     }
 
-    data class Order(val toppings: List<String>)
+    private fun getSavedOrders(): List<Order> {
+        val ordersJson =
+            getSharedPreferences("orders", Context.MODE_PRIVATE).getString("orders", "[]")
+        return Gson().fromJson(ordersJson, Array<Order>::class.java).toList()
+    }
+    private fun updateSavedOrders(orders: List<Order>) {
+        val editor = getSharedPreferences("orders", Context.MODE_PRIVATE).edit()
+        val ordersJson = Gson().toJson(orders)
+        editor.putString("orders", ordersJson)
+        editor.apply()
+    }
 
-    class OrderAdapter(context: Context, orders: List<Order>) : ArrayAdapter<Order>(context, R.layout.past_order, orders) {
+}
 
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.past_order, parent, false)
+data class Order(val toppings: List<String>, var isChecked: Boolean = false)
 
-            val toppingsTextView = view.findViewById<CheckedTextView>(R.id.past_order_item_checkbox)
+class OrderAdapter(
+    context: Context,
+    orders: List<Order>,
+    private val selectedOrderPosition: Int
+) : ArrayAdapter<Order>(context, R.layout.past_order, orders) {
 
-            val order = getItem(position)
-            toppingsTextView.text = order?.toppings!!.joinToString(", ")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context)
+            .inflate(R.layout.past_order, parent, false)
 
-            return view
-        }
+        val pastOrderCheckbox = view.findViewById<CheckedTextView>(R.id.past_order_item_checkbox)
+        val pastOrder = getItem(position) as Order
+        pastOrderCheckbox.text = pastOrder.toppings.joinToString(", ")
+        pastOrderCheckbox.isChecked = position == selectedOrderPosition
+        pastOrderCheckbox.isChecked = pastOrder.isChecked
+
+        return view
     }
 }
